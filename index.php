e6408bff399c250789e395d699c93ec916e70cb9
TimPasquel <146892252+TimPasquel@users.noreply.github.com>
2023-10-17 19:44:29 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Tim Pasquel Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>Welcome!</h1>
+	<?php include 'nav.html' ?>
+	<h2>This the parent site that has both my personal page as well as my media page</h2>
+</body>
+</html>
