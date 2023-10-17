3504cda8e548feccdea744b019da3edce999902a
TimPasquel <146892252+TimPasquel@users.noreply.github.com>
2023-10-17 19:45:42 -0400

    Create Tim_Pasquel.php

---
+++ Tim_Pasquel.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Tim Pasquel</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>Tim Pasquel </h1>
+	<?php include 'nav.html' ?>
+
+	<p>Hello! I am a student at Kutztown University studying for a BS in Information Technology as well as a certificate in Cybersecurity. I am aspiring to make my profession in information technology</p>
+
+	<h2>Work:</h2>
+	<p>Kutztown University Recreation Center Climbing Wall Associate</p>
+	<p>Automotive Racing Media Technician at Pocono Raceway</p>
+	<p>Grounds Operations/Maintenance at Morningstar Living</p>
+	<p>Stem Camp Counselor for Camp Invention</p>
+
+	<h2>Links:</h2>
+	<p><a href="https://www.linkedin.com/in/tim-pasquel-95363b259/"</a>LinkedIn</p>
+	<p><a href="https://github.com/TimPasquel"</a>Github</p>
+	<p><a href="https://www.instagram.com/capture_white_smoke/"</a>Instagram (@capture_white_smoke)</p>
+</body>
+</html>
