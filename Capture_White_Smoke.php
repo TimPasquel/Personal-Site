90c447fe0f5c78337e8f56bf0894c8ebe257f711
TimPasquel <146892252+TimPasquel@users.noreply.github.com>
2023-10-17 19:44:05 -0400

    Create Capture_White_Smoke.php

---
+++ Capture_White_Smoke.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Capture White Smoke</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>Capture White Smoke</h1>
+	<?php include 'nav.html' ?>
+	<h2>Welcome!</h2>
+	<p>Hello! Photographer who specializes in automotive culture, but love all subjects. Feel free to DM for inquires, questions, bookings, or image copies</p>
+</body>
+</html>
