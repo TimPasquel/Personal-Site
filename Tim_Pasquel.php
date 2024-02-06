8845b533fd21fb75c39eecead88a636aa9666238
TimPasquel <146892252+TimPasquel@users.noreply.github.com>
2024-02-06 17:18:28 -0500

    Add files via upload

--- Tim_Pasquel.php
+++ Tim_Pasquel.php
@@ -20,99 +20,40 @@
 		</ul>
 		</div>
 
-	<?php include 'TimNav.html' ?>
-
-<div class="TimBooks">
-
-<h2 id="work-experience">Work Experience</h2>
-<ul>
-<li><p><strong><a href="https://www.kutztown.edu/about-ku/administrative-offices/recreational-services/student-recreation-center-services-and-facilities.html">Kutztown University Recreation Center Climbing Wall Associate</a></strong> <span class="underline">[September 2023 - Present]</span></p>
-<p>Work with many new and experienced climbing customers. Ensure the safety of both the customers as well as fellow employees on and off the climbing wall. Teach new climbers how to safely climb.</p></li>
-<li><p><strong><a href="https://www.readysetdrift.com">Automotive Racing Media Technitian at Pocono Raceway</a></strong> <span class="underline">[July 2023 - Present]</span></p>
-<p>Work alongside with amateur race car drivers and drifters at Pocono Raceway. Capture and produce media. Proficient in Raceway safety and etiquette. Media Account: @capture_white_smoke.</p></li>
-<li><p><strong><a href="https://www.morningstarliving.org">Grounds Operations/Maintenance at Morningstar Living</a></strong> <span class="underline">[March 2023 - Present]</span></p>
-<p>Maintain the grounds of 124 housing units/52 acres at Heritage Village. Maintain housing units, equipment and appliances as well as taught and trained employees. Experience with using $3,000,000 worth of equipment, tools, and vehicles.</p></li>
-<li><p><strong><a href="https://www.invent.org/programs/camp-invention">Stem Camp Counselor for Camp Invention</a></strong> <span class="underline">[June 2018 - July 2018]</span></p>
-<p>Leader of student groups; responsible for the supervision and assistance of student groups, their classes, and offering support to teachers. Enhanced activity strategies to help students with different ability levels engage in activities. Supervised participants to spot difficulties and provide aid, stopping activities and offering medical assistance if needed.</p></li>
-</ul>
-<h2 id="computer-languages">Computer Languages</h2>
-<ul>
-<li><p>Java</p></li>
-<li><p>C++</p></li>
-<li><p>PHP</p></li>
-<li><p>HTML</p></li>
-<li><p>CSS</p></li>
-<li><p>SQL</p></li>
-<li><p>Python</p></li>
-<li><p>Shell Scripting</p></li>
-</ul>
-<h2 id="software-applications">Software Applications</h2>
-<ul>
-<li><p>Unix</p></li>
-<li><p>Windows</p></li>
-<li><p>Macintosh</p></li>
-<li><p>Mint Linux</p></li>
-<li><p>Linux</p></li>
-<li><p>Vim</p></li>
-<li><p>Nano</p></li>
-<li><p>Wireshark</p></li>
-<li><p>VeraCrypt</p></li>
-</ul>
-<h2 id="toolsskills">Tools/Skills</h2>
-<ul>
-<li><p>Video editing and processing</p></li>
-<li><p>General computer construction and maintenance</p></li>
-<li><p>Patch cable and ethernet cable splicing</p></li>
-<li><p>Small engine maintenance</p></li>
-<li><p>Appliance maintenance</p></li>
-<li><p>Landscaping</p></li>
-<li><p>Mowing</p></li>
-<li><p>Trimming</p></li>
-</ul>
-<h2 id="organizations">Organizations</h2>
-<ul>
-<li><p>Kutztown Technology Association (KTA): 2021-Present</p></li>
-<li><p>Kutztown University Honors Program: 2021-Present</p></li>
-<li><p>Kutztown Stem Club: 2023-Present</p></li>
-<li><p>Photographer for ReadySetDrift at Pocono Raceway: 2023-Present</p></li>
-<li><p>Kutztown University Marching Unit: 2021-2023</p></li>
-<li><p>Kutztown University Esports Team: 2021-2022</p></li>
-</ul>
-
-</div>
-
-
-<!--
-	
 	<h2>Work:</h2>
 		<div class='TimEXP'>
 		<table>
 			<tr>
-				<td><b>Title:</b></td>
-				<td><b>Description:</b></td>
+				<td>Title</td>
+				<td>Description</td>
+				<td>Time</td>
 			</tr>
 			<tr>
 				<td><a href="https://www.kutztown.edu/about-ku/administrative-offices/recreational-services/student-recreation-center-services-and-facilities.html">Kutztown University Recreation Center Climbing Wall Associate</a></td>
-				<td><u>[September 2023 - Present]</u> <br> Work with many new and experienced climbing customers. Ensure the safety of both the customers as well as fellow employees on and off the climbing wall. Teach new climbers how to safely climb</td>
+				<td>Work with many new and experienced climbing customers. Ensure the safety of both the customers as well as fellow employees on and off the climbing wall. Teach new climbers how to safely climb</td>
+				<td>September 2023 - Present</td>
 			</tr>
 			<tr>
 				<td><a href="https://www.readysetdrift.com">Automotive Racing Media Technician at Pocono Raceway</a></td>
-				<td><u>[July 2023 - Present]</u> <br> Work alongside with amateur race car drivers and drifters at Pocono Raceway. Capture and produce media. Proficient in Raceway safety and etiquette. Media Account: @capture_white_smoke</td>
+				<td>Work alongside with amateur race car drivers and drifters at Pocono Raceway. Capture and produce media. Proficient in Raceway safety and etiquette. Media Account: @capture_white_smoke</td>
+				<td>July 2023 - Present</td>
 			</tr>
 			<tr>
 				<td><a href="https://www.morningstarliving.org">Grounds Operations/Maintenance at Morningstar Living</a></td>
-				<td><u>[March 2023 - Present]</u> <br> Maintain the grounds of 124 housing units/52 acres at Heritage Village. Maintain housing units, equipment and appliances as well as taught and trained employees. Experience with using $3,000,000 worth of equipment, tools, and vehicles</td>
+				<td>Maintain the grounds of 124 housing units/52 acres at Heritage Village. Maintain housing units, equipment and appliances as well as taught and trained employees. Experience with using $3,000,000 worth of equipment, tools, and vehicles</td>
+				<td>March 2020 - Present</td>
 			</tr>
 			<tr>
 				<td><a href="https://www.invent.org/programs/camp-invention">Stem Camp Counselor for Camp Invention</a></td>
-				<td><u>[June 2018 - July 2018]</u> <br> Leader of student groups; responsible for the supervision and assistance of student groups, their classes, and offering support to teachers. Enhanced activity strategies to help students with different ability levels engage in activities. Supervised participants to spot difficulties and provide aid, stopping activities and offering medical
+				<td>Leader of student groups; responsible for the supervision and assistance of student groups, their classes, and offering support to teachers. Enhanced activity strategies to help students with different ability levels engage in activities. Supervised participants to spot difficulties and provide aid, stopping activities and offering medical
 assistance if needed.</td>
+				<td>June 2018 - July 2018</td>
 			</tr>
 		</table>
 		</div>
 
 	<br />
-	<br /> 
+	<br /> <!--3 lines empty space -->
 	<br />
 
 	<table class='TimTable'>
@@ -180,7 +121,7 @@ assistance if needed.</td>
 	</td></tr>
 	</table>
 	
--->
+	<?php include 'TimNav.html' ?>
 
 	<h2>Personal External Links/Friends Links:</h2>
 		<div class="CWSLinkBox">
